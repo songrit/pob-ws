ce4ab284b8c8629295ad5910c173ba02ee32f932
songrit <songrit@gmail.com>
2011-03-25 16:45:40 +0700

    /api/hotel_descriptive_content_notif

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -21,8 +21,26 @@ class ApiController < ApplicationController
   def hotel_descriptive_content_notif
     doc = Nokogiri::XML(request.body)
     LogRequest.log(request,doc.to_s)
-#    render :text => "hello, #{doc.xpath('//xmlns:HotelDescriptiveContent').attribute('HotelName').value}"
-    render :layout => false 
+    # l= LogRequest.find 7
+    # doc = Nokogiri::XML(l.content)
+    hotel= Hotel.new :code=> doc.xpath("//xmlns:HotelDescriptiveContent").attribute("HotelCode").value,
+      :name => doc.xpath("//xmlns:HotelDescriptiveContent").attribute("HotelName").value,
+      :brand_code => doc.xpath("//xmlns:HotelDescriptiveContent").attribute("BrandCode").value,
+      :brand_name => doc.xpath("//xmlns:HotelDescriptiveContent").attribute("BrandName").value,
+      :currency_code => doc.xpath("//xmlns:HotelDescriptiveContent").attribute("CurrencyCode").value,
+      :info_updated_on => doc.xpath("//xmlns:HotelInfo").attribute("LastUpdated").value,
+      :hotel_status_code => Hotel.status(doc.xpath("//xmlns:HotelInfo").attribute("HotelStatus").value),
+      :lat => doc.xpath("//xmlns:Position").attribute("Latitude").value.to_f,
+      :lng => doc.xpath("//xmlns:Position").attribute("Longitude").value.to_f,
+      :address => doc.xpath("//xmlns:AddressLine").text,
+      :city_name => doc.xpath("//xmlns:CityName").first.text,
+      :postal_code => doc.xpath("//xmlns:PostalCode").text,
+      :state_prov => doc.xpath("//xmlns:StateProv").first.text, 
+      :country_name => doc.xpath("//xmlns:CountryName").first.text,
+      :description => doc.xpath('//xmlns:TextItem[@Title="Description"]').xpath('xmlns:Description').text
+    hotel.save
+    response.content_type = "application/xml"
+    render :layout => false
   end
   def post
     doc = Nokogiri::XML(request.body)
