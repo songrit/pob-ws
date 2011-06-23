3b6a5d77ee203dc1bb71ee44db4e5da5e9f27d1e
songrit <songrit@gmail.com>
2011-06-23 15:50:55 +0700

    fix /main/search

---
+++ config/initializers/elocal.rb
@@ -0,0 +1,27 @@
+# lao specific
+PROVINCE_ID= 3
+DISTRICT_ID= 364
+# for lao with multiple sub_districts, do not define this constant
+SUB_DISTRICT_ID= 426
+# maximum number of moo (village)
+MAX_MOO= 5
+
+LAAS_USER = "abtbtnai714"
+LAAS_PASSWORD = "318883"
+LAAS_RETRY = 3
+# GmaUser for permanent secretary
+MAYOR = 3
+PALAD = 4
+FINANCE_HEAD = 5
+# GmaUser for income summary person
+INCOME_SUMMARY_MAKER = 2
+
+FINANCE_SECTION = 2
+
+# role for security officer (can search for everything)
+SECURED_ROLE = 'S'
+PER_PAGE = 25
+
+ACCOUNT_TYPE = ['สินทรัพย์','เจ้าหนี้','ทุน','รายรับ','รายจ่าย']
+MONTHS= ['มกราคม', 'กุมภาพันธ์', 'มีนาคม', 'เมษายน', 'พฤษภาคม', 'มิถุนายน', 'กรกฏาคม', 'สิงหาคม', 'กันยายน', 'ตุลาคม', 'พฤศจิกายน', 'ธันวาคม']
+  
