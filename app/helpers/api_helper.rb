8ac0fa786a4833b1236f56c6ae83eccda31dc036
songrit <songrit@gmail.com>
2011-08-22 17:25:29 +0700

    fix hotel_res

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -1,14 +1,14 @@
 module ApiHelper
   # check if avails cover from start date to end date
   def cover_stay_range(hotel,start_on,end_on)
-    cover= true
+    @cover= true
     start_on.upto(end_on-1) do |d|
       a= Availability.last :conditions=>['hotel_id=? AND limit_on=?',hotel.id,d]
       unless (a && a.limit>0)
-        cover= false
+        @cover= false
         break
       end
     end
-    return cover
+    return @cover
   end
 end
